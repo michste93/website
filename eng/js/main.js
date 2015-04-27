7ff5fa60d9fd48079a7b14be8c01a2e684bcd7b2
Stefano <michste93@gmail.com>
2015-04-27 12:57:01 +0200

    first commit

---
+++ eng/js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+	 //on mobile - open/close primary navigation clicking/tapping the menu icon
+	$('.cd-primary-nav').on('click', function(event){
+		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
+});
