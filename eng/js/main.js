5382973fe22b26fb10c7ab222b295fb892f7c62a
Stefano <michste93@gmail.com>
2015-05-15 09:55:36 +0200

    fixes

--- eng/js/main.js
+++ eng/js/main.js
@@ -1,24 +1,22 @@
 jQuery(document).ready(function($){
-     var tabs = document.getElementsByClassName('#cd-intro-fix');
-     var url = window.location.href;
+ /*    var url = window.location.href;
      if (url.charAt(url.length-1) == '/') {
          url = url.substr(0,url.length - 1);
    }
    var curPage = url.split('/').pop().toLowerCase();
-   $('#cd-intro-fix::after').css({"background":"transparent url(../img/winter.jpg) no-repeat;"});
-
-for (i=0; i<tabs.length; i++){
-    if (tabs[i].getAttribute('data-link').toLowerCase() == "winter.html"){
-        '#cd-intro-fix'.style.backgroundImage = 'url(../img/winter.jpg)';
-    }
-    if (tabs[i].getAttribute('data-link').toLowerCase() == "summer.html"){
-        '#cd-intro-fix'.style.backgroundImage = 'url(../img/summer.jpg)';
-    }
-}
    
+   
+ if (curPage.toLowerCase() == "summer.html"){
+        $('head').append('<style>#cd-intro-fix::after{background: transparent url(../img/summer.jpg) no-repeat;}</style>');
+        //document.getElementById("cd-intro-fix::after").style.backgroundImage="url('img/summer.jpg')";
+    }
+   */ 
+
 	 //on mobile - open/close primary navigation clicking/tapping the menu icon
 	$('.cd-primary-nav').on('click', function(event){
 		if($(event.target).is('.cd-primary-nav')) 
 		   $(this).children('ul').toggleClass('is-visible');
+		   
 	});
 });
+
